fb9d4f9c137e76468b516ced221f6f970cedeef0
speryodi <songagreen@gmail.com>
2020-04-07 03:05:42 +0900

    Object detection using Yolo3

---
+++ main.py
@@ -0,0 +1,75 @@
+import cv2
+import numpy as np
+
+#Load Yolo
+net = cv2.dnn.readNet("yolov3-tiny.weights", "yolov3-tiny.cfg")
+classes = []
+
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+# colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+# Load img
+img = cv2.imread("test.jpeg")
+img = cv2.resize(img, None, fx = 0.8, fy = 0.8)
+height, width, channels = img.shape
+
+# Detect objects
+blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+net.setInput(blob)
+outs = net.forward(output_layers)
+
+#Show information on the screen
+class_ids = []
+confidences = []
+boxes = []
+
+for out in outs:
+    for detection in out:
+        # detect the confidence, How correct the algorithm
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        #scores를 최댓값으로 만들기 위한
+        confidence = scores[class_id]
+        if confidence > 0.2:
+            # 객체 인식
+            center_x = int(detection[0] * width)
+            center_y = int(detection[1] * height)
+            w = int(detection[2] * width)
+            h = int(detection[3] * height)
+
+            # Rectangle coordinate, 사각형 좌표
+            x = int(center_x - w / 2)
+            y = int(center_y - h / 2)
+
+            boxes.append([x, y, w, h])
+            confidences.append(float(confidence))
+            class_ids.append(class_id)
+
+# confidence가 더 높은 boxes
+indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+font = cv2.FONT_HERSHEY_PLAIN
+for i in range(len(boxes)):
+    if i in indexes:
+        x, y, w, h = boxes[i]
+        label = classes[class_ids[i]]
+        print(label)
+        cv2.rectangle(img, (x,y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(img, label, (x, y + 30), font, 1, (255, 255, 255), 1)
+
+
+
+
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+
+
